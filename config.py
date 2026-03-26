8af659e6b9ad8a7ef1b1e5a0bb5385b34e841273
9pvbloo <i2413131@continental.edu.pe>
2026-03-26 14:19:06 -0500

    fix railway mysql connection

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "090806")
 
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "MYSQL_PUBLIC_URL",
-        "mysql+mysqlconnector://root:090806@localhost:3306/veza_portal_db"
+        "DATABASE_URL",
+        "mysql+mysqlconnector://root:xLnWyOjMHoSwYjCfNQjqciACrbFSvzar@shuttle.proxy.rlwy.net:52430/railway"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
